6b956f9b91b6f4a93f0d6b14469ad0c1a86d835a
Khizar Atif <khizaratif.ai@gmail.com>
2026-07-01 18:04:29 +0500

    successfully added the read and write feature
    i have now added the feature in first the program reads the existing todos and than adds the other todos next to them however this feature have a few bugs for now will fix them later on

--- TO-DO_App/main.py
+++ TO-DO_App/main.py
@@ -1,5 +1,3 @@
-todos = []
-
 while True:
     user_action = input("Type 'Add', 'Show', 'Edit', 'Complete' or 'Exit' : ")
     user_action = user_action.capitalize()
@@ -9,14 +7,21 @@ while True:
         case 'Add':
             todo = input("Enter a todo: ") + "\n"
             todo = todo.capitalize()
+
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+
             file = open('todos.txt', 'w')
             file.writelines(todos)
+            file.close()
         case 'Show':
-            if not todos:
-                print("No todos are added!")
-            if todos:
-             print("\n" + f"--------------YOUR TODO LIST HAVE {len(todos)} ITEMS--------------")
+#            if not todos:
+#               print("No todos are added!")
+#            if todos:
+             print("\n" + f"--------------YOUR TODO LIST--------------")
              for index, item in enumerate(todos, start = 1):
                 row = f"{index}. {item}"
                 print(row)
@@ -26,7 +31,7 @@ while True:
             new_todo = input("Enter new todo: ")
             todos[number - 1] = new_todo
             print("\n""YOUR TODO LIST IS SUCCESSFULLY UPDATED!")
-            print("\n" + f"--------------YOUR TODO LIST HAVE {len(todos)} ITEMS--------------")
+            print("\n" + f"--------------YOUR TODO LIST HAVE--------------")
             for index, item in enumerate(todos, start = 1):
                 row = f"{index}. {item}"
                 print(row)
