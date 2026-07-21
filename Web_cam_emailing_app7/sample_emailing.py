fe41bff94c6730eaab4e61ef51c7066665147ae8
Khizar Atif <khizaratif.ai@gmail.com>
2026-07-21 16:51:46 +0500

    added the sample emailing python file

---
+++ Web_cam_emailing_app7/sample_emailing.py
@@ -0,0 +1,67 @@
+import smtplib
+from email.message import EmailMessage
+import os
+
+
+SENDER = "ENTER_YOUR_OWN_EMAIL_HERE"
+PASSWORD = "ENTER_THE_16_DIGIT_PASSWORD_HERE"
+RECEIVER = "ENTER_THE_RECEIVER_EMAIL_HERE"
+
+
+def send_email(image_path):
+
+    if not os.path.exists(image_path):
+        print("Image not found")
+        return
+
+
+    print("Sending email...")
+
+
+    email = EmailMessage()
+
+    email["Subject"] = "Security Alert: Person Detected"
+    email["From"] = SENDER
+    email["To"] = RECEIVER
+
+
+    email.set_content(
+        "A person has been detected by the security camera. Image attached."
+    )
+
+
+    with open(image_path, "rb") as file:
+
+        image_data = file.read()
+
+
+    email.add_attachment(
+        image_data,
+        maintype="image",
+        subtype="jpeg",
+        filename="intruder.jpg"
+    )
+
+
+    try:
+
+        with smtplib.SMTP_SSL(
+            "smtp.gmail.com",
+            465
+        ) as smtp:
+
+            smtp.login(
+                SENDER,
+                PASSWORD
+            )
+
+            smtp.send_message(email)
+
+
+        print("Email sent successfully!")
+
+
+    except Exception as error:
+
+        print("Email error:")
+        print(error)
